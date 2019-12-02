2be4b0541451f7d56b433e19e7d7417636c96e79
antklim <antklim@gmail.com>
2019-12-02 22:39:53 +1100

    Should panic test added

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -4,8 +4,8 @@
 #![test_runner(antklimos::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-use core::panic::PanicInfo;
 use antklimos::{println, serial_print, serial_println};
+use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
